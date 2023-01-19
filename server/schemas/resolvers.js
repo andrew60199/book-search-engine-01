8e313185c201e8a76055ee51f8be7dc4b4f130a4
AND.EVV <andrew60199@gmail.com>
2023-01-19 12:43:50 +1100

    began writing up the resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,16 +5,48 @@ const { signToken } = require('../utils/auth');
 const resolvers = {
   Query: {
     // Need to get the server linked so I can test the work I do... 
-    user: async (parent, { user }) => {
-      return User.findOne({
-        $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
-      })
-    }
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate('savedBooks');
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
   },
 
-  // Mutation: {
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
-  // },
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { authors, description, title, bookId, image,link }, context) => {
+      if (context.user) {
+
+      }
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        
+      }
+    }
+  }
 };
 
 module.exports = resolvers;
